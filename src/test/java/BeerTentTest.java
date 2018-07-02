585550f60b0c9f3ee0f83cff7543f4776580761b
jamos90 <jamos_98@hotmail.com>
2018-07-02 16:17:44 +0100

    beer tent tests passing

---
+++ src/test/java/BeerTentTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BeerTentTest {
+
+    BeerTent beerTent;
+
+    @Before
+    public void setUP(){
+        beerTent = new BeerTent("Brauhaus","Heir Hanz",45);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Brauhaus", beerTent.getName());
+    }
+
+    @Test
+    public void hasOwner(){
+        assertEquals("Heir Hanz", beerTent.getOwner());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals(45,beerTent.getParkingSpot());
+    }
+}
