4405c568b5a416e26ac95a94206a461bb28eab61
jamos90 <jamos_98@hotmail.com>
2018-07-02 15:53:43 +0100

    dodgems creatd and name test passed

---
+++ src/test/java/DodgemsTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void setUp(){
+        dodgems = new Dodgems("Red Shell Attack");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Red Shell Attack", dodgems.getName());
+    }
+
+}
