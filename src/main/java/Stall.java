dcfa7a0f7129edab594dbc2ed7e49eb5f87e0f8d
jamos90 <jamos_98@hotmail.com>
2018-07-02 16:06:45 +0100

    stall abstract class created

--- src/main/java/Stall.java
+++ src/main/java/Stall.java
@@ -1,2 +1,13 @@
-public class Stall {
+public abstract class Stall {
+    protected String name;
+    protected String owner;
+    protected int parkingSpot;
+
+    public Stall(String name, String owner, int parkingSpot){
+        this.name = name;
+        this.owner = owner;
+        this.parkingSpot = parkingSpot;
+    }
+
+
 }
